065fed46f3fe5c7cf9d39061ae7394112b4076ec
Varun <113620756+Varun114-Techno@users.noreply.github.com>
2023-01-11 20:04:00 +0530

    Add files via upload

---
+++ play.php
@@ -0,0 +1,59 @@
+<?php
+$q=$_GET['videoUrl'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" href="https://img.icons8.com/doodle/480/000000/circled-play.png" type="image/gif" sizes="16x16">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="https://cdn.fluidplayer.com/v3/current/fluidplayer.min.js"></script>
+<title>Varun114 Techno</title>
+</head>
+<body>
+	<center>
+	<h1 class='postTitle'>
+<span>
+VARUN114-PLAYER
+</span>
+</h1>
+<div class='postInner'>
+<div class='postEntry' id='postID-1323700241837967924'>
+<div class='postBody' id='postBody'><style>
+	h1 { color: DodgerBlue; font-family: 'Splash', cursive; font-size: 42px; font-weight: 800; line-height: 72px; margin: 0 0 24px; text-align: center; text-transform: uppercas
+</style></center>
+<video id="video-id" src="https://pubads.g.doubleclick.net/ssai/event/<?php echo $q; ?>" type="application/vnd.apple.mpegurl">
+<script>
+    var myFP = fluidPlayer(
+        'video-id',	{
+	"layoutControls": {
+		"controlBar": {
+			"autoHideTimeout": 3,
+			"animated": true,
+			"autoHide": true
+		},
+		"htmlOnPauseBlock": {
+			"html": null,
+			"height": null,
+			"width": null
+		},
+		"autoPlay": true,
+		"mute": true,
+		"allowTheatre": false,
+		"playPauseAnimation": false,
+		"playbackRateEnabled": false,
+		"allowDownload": false,
+		"playButtonShowing": false,
+		"fillToContainer": true,
+		"posterImage": "https://i.postimg.cc/vT2qvD6w/1652236607904-removebg.png"
+	},
+	"vastOptions": {
+		"adList": [],
+		"adCTAText": false,
+		"adCTATextPosition": ""
+	}
+});
+</script>
+</body>
+</html>
